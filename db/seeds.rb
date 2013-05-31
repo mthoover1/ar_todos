069f9fec82d984f28024b1bda0b911a98d8590ec
Liz Abinante <eabinante@gmail.com>
2013-05-31 11:32:18 -0500

    list.id modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ require 'faker'
 list = List.create(:name => 'Home')
 
 10.times do
-  Task.create(:name => Faker::Lorem.sentence(word_count = 4), :list_id => list)
+  Task.create(:name => Faker::Lorem.sentence(word_count = 4), :list_id => list.id)
 end
 
