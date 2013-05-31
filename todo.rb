02437623d948764ba45e8314b1bd5a785507fff0
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 13:22:58 -0500

    Added all_todos method to todo.rb

--- todo.rb
+++ todo.rb
@@ -1,9 +1,11 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
 
 def all_todos
   Task.all.each do |task|
     puts "#{task.id}. #{task.name}"
   end
 end
+
+all_todos if ARGV[0] == 'list'
