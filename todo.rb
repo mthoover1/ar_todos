bc465cc6da511c2aac43a4ce3af17984af77d4ba
Liz Abinante <eabinante@gmail.com>
2013-05-31 13:13:44 -0500

    added all_todos method

--- todo.rb
+++ todo.rb
@@ -1,3 +1,9 @@
 require_relative 'config/application'
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+def all_todos
+  Task.all.each do |task|
+    puts "#{task.id}. #{task.name}"
+  end
+end
